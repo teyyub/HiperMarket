7adceafca0c2b2b0263a925a5ed4313be68396b2
Aliheydar Rustamov <alihaydarustamov92@gmail.com>
2021-03-31 12:35:09 +0400

    commit second

--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,7 @@ namespace WinFormsApp
         {
             Console.WriteLine("test console: ");
             Console.WriteLine("test console: ");
+            Console.WriteLine("test console: ");
         }
 
         private void button1_Click(object sender, EventArgs e)
